d0b530a5eac9bdd95bbad62647f76c1246f5a804
Lingsheng Dong <lingsheng_dong@hms.harvard.edu>
2024-03-25 08:34:44 -0400

    oot not work, even requeue when time is low

--- scripts/bashScriptV210.sh
+++ scripts/bashScriptV210.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
 outputs=""
-for i in {1..3}; do
+for i in {5..5}; do
     input=bigText$i.txt
     output=1234.$i.txt
     #@1,0,useMemTimeWithInput10,,input,sbatch -p short -c 1 --mem 2G -t 50:0 
     useMemTimeWithInput10.sh $input; grep 1234 $input > $output
     outputs=$outputs,$output
     
-    output=5678.$i.txt
-    #@2,0,useMemTimeWithInput10,,input,sbatch -p short -c 1 --mem 2G -t 50:0
-    useMemTimeWithInput10.sh $input; grep 5678 $input > $output
-    outputs=$outputs,$output
+    # output=5678.$i.txt
+    # #@2,0,useMemTimeWithInput10,,input,sbatch -p short -c 1 --mem 2G -t 50:0
+    # useMemTimeWithInput10.sh $input; grep 5678 $input > $output
+    # outputs=$outputs,$output
 done
 
 input=bigText1.txt
